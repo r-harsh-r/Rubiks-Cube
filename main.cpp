334a0d7d482118497844a43a7e5980136acf5a87
r-harsh-r <harshr@kgpian.iitkgp.ac.in>
2025-06-04 18:10:52 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// abstract class
+class RubiksCube{
+    public:
+    
+    virtual void roatation() = 0;
+    virtual void print() = 0;
+    virtual void RandomShuffle() = 0;
+    virtual void isSolved() = 0;
+    
+
+};
+
+
+int main(){
+
+    return 0;
+}
